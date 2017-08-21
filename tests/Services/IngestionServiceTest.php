41f3f8288d4e0260057c0a64b7c36d1065bbfb0d
krdinesh <dinesh@radiansys.com>
2017-08-21 14:15:02 +0530

    Added Basic header condition while creating ingestion Service

--- tests/Services/IngestionServiceTest.php
+++ tests/Services/IngestionServiceTest.php
@@ -7,7 +7,7 @@ use Krdinesh\Greenhouse\GreenhousePhp\Services\IngestionService;
 class IngestionApiServiceTest extends \PHPUnit_Framework_TestCase {
 
     public function setUp(){
-        $this->ingestionService = new IngestionService('apiKey');
+        $this->ingestionService = new IngestionService('apiKey',false);
         $this->errorService = new IngestionService('exception_api_key');
         $this->baseUrl =  IngestionService::ingestionBaseUrl();
     }
